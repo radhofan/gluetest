d41f9f457ea8579655de56d253ae9144717f4e0b
Radhofan <radhofanazizi@gmail.com>
2025-02-21 18:48:11 +0700

    ada

---
+++ reproduce.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Install Miniconda
+curl -fsSL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+
+# Set environment variables
+export PATH="$HOME/miniconda/bin:$PATH"
+export MAMBA_ROOT_PREFIX="$HOME/miniconda"
+
+# Install Mamba
+conda install -c conda-forge mamba -y
+
+# Initialize Mamba shell
+mamba shell init --shell=bash
+source ~/.bashrc  # Reload shell config
+eval "$(mamba shell hook --shell=bash)"
+
+# Install zip
+sudo apt install zip
+
+# Setting up graalvm and python 
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install java 17.0.7-graal
+gu install python
+
+# Run test
+sdk install maven
+mamba create --name gluetest python=3.11.4
+mamba init
+source ~/.bashrc
+mamba activate gluetest
+mamba install pytest
+bash gluetest/run.sh
